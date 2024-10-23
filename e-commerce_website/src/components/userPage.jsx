3e0cd31a80f04c299a05bd9c38761ebb7babe569
Prahit Yaugand <prahityaugand@wirelessprv-10-192-72-155.near.illinois.edu>
2024-10-23 08:58:29 -0500

    Favorites and Profile info added

--- e-commerce_website/src/components/userPage.jsx
+++ e-commerce_website/src/components/userPage.jsx
@@ -11,7 +11,12 @@ function UserPage(props) {
   <Search>
     
   </Search>
-  <GetPastTransactions transactionData={props.history} /></>
+  <GetPastTransactions transactionData={props.history} /> 
+  <div style={{ paddingTop: '20px' }}> 
+    <GetFavProducts favProducts={props.favorite} />
+  </div>
+  <UserInfo info={props.user}/>
+  </>
   );
 
 }
@@ -22,24 +27,56 @@ function GetPastTransactions({transactionData}) {
   const dates = transactionData[2];
 
   const productRows = [];
-  for (let i = 0; i < products.length; i++) {
+  for (var i = 0; i < products.length; i++) {
     productRows.push(
-      <Transaction product={products[i]} price ={prices[i]} date={dates[i]}/>
+      <Transaction product={products[i]} price={prices[i]} date={dates[i]}/>
     );
   }
   return (
     <div>
       <h2>History of Transactions:</h2>
-      <table>
+      <table className ="transactionTable">
         <thead>
           <tr>
-            <th >Product  </th>
-            <th >Price  </th>
+            <th>Product </th>
+            <th>Price </th>
             <th>Date</th>
           </tr>
         </thead>
         <tbody>
           {productRows}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function GetFavProducts({favProducts}) {
+  const favNames = favProducts[0];
+  const favPrices = favProducts[1];
+  const favDates = favProducts[2];
+
+  const favProductRows = [];
+  for (var i = 0; i < favProducts.length; i++) {
+    favProductRows.push(<Transaction product={favNames[i]} 
+                        price={favPrices[i]}
+                        date={favDates[i]}/>);
+  };
+  return (
+    <div>
+      <h2>
+        Favorite Products:
+      </h2>
+      <table className = "transactionTable" style={{ marginTop: '10px' }}>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {favProductRows}
         </tbody>
       </table>
     </div>
@@ -56,6 +93,24 @@ function Transaction(props) {
     );
 }
 
+
+function UserInfo(props){
+  // name, email
+  return(
+  <div className= "userInfo">
+    <h2>Profile Information:</h2>
+  
+      <p>
+      <b> Name: </b> {props.info[0]}
+      </p>
+      <p>
+      <b> Email: </b> {props.info[1]}
+      </p>
+    
+    
+  </div>
+  );
+}
 
 
 
