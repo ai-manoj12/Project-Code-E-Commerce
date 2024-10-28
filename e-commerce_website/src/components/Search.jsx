07fa4d60ffd71fb7d9eb043eadb4b61a964449e8
Prahit Yaugand <prahityaugand@wirelessprv-10-192-72-155.near.illinois.edu>
2024-10-28 12:11:07 -0500

    Updated user page

--- e-commerce_website/src/components/Search.jsx
+++ e-commerce_website/src/components/Search.jsx
@@ -12,14 +12,20 @@ function Search() {
       setInput(e.target.value);
     }
   }
-  return (<> <input
-    type="text"
-    value = {input}
-    className="search"
-    placeholder="Search"
-    onChange={inputHandler}
-  />
-  <button className='searchbutton' onClick={clickHandler}>Search</button> 
+  return (
+  
+  <> 
+  <div>
+    <input
+      type="text"
+      value = {input}
+      className="search"
+      placeholder="Search"
+      onChange={inputHandler}
+    />
+    <button className='searchbutton' onClick={clickHandler}>Search</button> 
+  </div>
+  
   </>)
 }
 
@@ -32,13 +38,4 @@ function clickAction({value}) {
 
 export default Search;
 
-
-//favorites, move everything down, 
-
-//favoriting a product
-//name, netid
-//changing email (need email from login. Add a button saying "Change Email". Then it lets should let user type in a)
-
-//profile image if someone set it up
-// what products that sellers are selling
 
