6f73244f3ddcf5ca917ecbdcd15c16caeeb43100
Gabe Zeller <86267493+gabezeller@users.noreply.github.com>
2025-04-20 15:46:32 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Your Vite frontend
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
